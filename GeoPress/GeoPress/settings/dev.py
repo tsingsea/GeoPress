9b58f83ee1fac6c2811f5e030241eb2ae69c5c8d
TSingSea <59871143+TSingSea@users.noreply.github.com>
2022-02-13 19:47:21 +0800

    Update settings.dev
    - pymysql connect
    - update DATABASES

--- GeoPress/GeoPress/settings/dev.py
+++ GeoPress/GeoPress/settings/dev.py
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'GeoPress.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rjxuf15q',
+        'USER': 'rJXUf15Q',
+        'PASSWORD': 'u9rGgQbZxJfq0x',
+        'HOST': '47.99.55.57',
+        'PORT': '3306',
     }
 }
 
